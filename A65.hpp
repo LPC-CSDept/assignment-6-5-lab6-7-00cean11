915c879ddbda4468d1470d0b462e234206a05cfc
Tengis Erdenebayar <oceanisnotsea@gmail.com>
2022-10-04 20:01:20 -0700

    A65

--- A65.hpp
+++ A65.hpp
@@ -3,7 +3,40 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-
+void getInput(int &a, int &b, int &c);
+void swap(int &a, int &b, int &c);
+void swaptwo(int &a, int &b);
+int main (){
+    int a, b, c;
+    getInput(a, b, c);
+    swap(a, b, c);
+    cout << "(" << a << ", " << b << ", " << c << ")" <<endl; 
+    swaptwo(a, b);
+    cout << "(" << a << ", " << b << ", " << c << ")" <<endl;
+}
+void getInput(int &num1, int &num2, int &num3) {
+  while (true) {
+    cout << "Enter your value: ";
+    cin >> num1 >> num2 >> num3;
+    if ((num1 > 0) && (num2 > 0) && (num3 > 0))
+      break;
+  }
+}
+void swap(int &a, int &b, int &c){
+    int temp;
+    temp = b;
+    b = a;
+    a = c;
+    c = temp;
+    swaptwo (a, b);
+  
+}
+void swaptwo(int &a, int &b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 // complete two functions
 // swap() ; to swap all three values like (a, b, c) to (c, a, b)
 //              This function will use swaptwo().
